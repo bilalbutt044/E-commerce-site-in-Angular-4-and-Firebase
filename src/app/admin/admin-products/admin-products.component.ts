3e8a9357a210545be3cb63f34b97c55cd3ee732c
bilal <bilal.soft000@gmail.com>
2019-03-13 19:46:12 +0500

    search for products in product array

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../../product.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: Product[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
-  constructor(private product: ProductService) {
-    this.products$ = this.product.getAll();
+  constructor(private productService: ProductService) {
+    this.subscription = this.productService.getAll()
+      .subscribe(product => this.products = product);
    }
+
+  
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
   ngOnInit() {
   }
