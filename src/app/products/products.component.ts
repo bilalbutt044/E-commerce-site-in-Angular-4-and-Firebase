904eb5fd3324801f0421456780588dc1f4ae94d2
bilal <bilal.soft000@gmail.com>
2019-03-14 18:01:23 +0500

    display products on homepage without selecting category

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-products',
@@ -8,10 +11,30 @@ import { CategoryService } from '../category.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   categories$;
-  constructor(private productService: ProductService, private categories: CategoryService) { 
-    this.products$ =  this.productService.getAll();
+  category: string;
+
+  constructor( 
+      private route: ActivatedRoute,
+      private productService: ProductService,
+      private categories: CategoryService) {
+
+    this.productService
+      .getAll()
+      .switchMap(product => {
+      this.products = product;
+      return this.route.queryParamMap;
+      })
+      .subscribe(params => {
+        this.category =  params.get('category');
+  
+        this.filteredProducts = (this.category) ? 
+          this.products.filter(p =>  p.category === this.category) :
+          this.products;
+      });
+    
     this.categories$ =  this.categories.getAll();
   }
 
