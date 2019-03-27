7c2d4a83415fbf75ab39aa5633d394512c620fc5
bilal <bilal.soft000@gmail.com>
2019-03-27 15:38:35 +0500

    design shopping cart page

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,14 +1,30 @@
 import { ShoppingCartItem } from "./shopping-cart-item";
 
 export class ShoppingCart {
-  
+  items: ShoppingCartItem[] = [];
 
-    constructor(public items: ShoppingCartItem[]) {}
-
-    get totalItemsCount() {
-       let count = 0;
-        for(let productId in this.items)
-          count += this.items[productId].quantity;
-        return count;
+  constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) { 
+    for (const productId in itemsMap) {
+      let item = itemsMap[productId];
+      this.items.push(new ShoppingCartItem(item.product, item.quantity)) ;
     }
+  }
+
+  get productIds() {
+    
+    return Object.keys(this.itemsMap);
+  }
+  get totalItemsCount() {
+    let count = 0;
+    for(let productId in this.itemsMap) 
+      count += this.itemsMap[productId].quantity;
+    return count;
+  }
+
+  get totalPrice() {
+    let sum = 0;
+    for (let productId in this.items)
+      sum += this.items[productId].totalPrice;
+    return sum;
+  }
 }
