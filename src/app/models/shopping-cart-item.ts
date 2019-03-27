a9714a9f1b8029fa34b400ad34ad40975396a06a
bilal <bilal.soft000@gmail.com>
2019-03-27 18:17:46 +0500

    added new poroperties in shoppingCartItem object So that we get $key value for shopping cart component and can print quantity in shopping cart page

--- src/app/models/shopping-cart-item.ts
+++ src/app/models/shopping-cart-item.ts
@@ -1,8 +1,14 @@
 import { Product } from "./product";
 
 export class ShoppingCartItem {
+    $key: string;
+    title: string;
+    imageUrl: string;
+    price: number;
+    quantity: number;
 
-    constructor(public product: Product, public quantity: number) {}
-
-    get totalPrice() { return this.product.price * this.quantity }; 
+    constructor(init?: Partial<ShoppingCartItem>) {
+        Object.assign(this, init);
+    }
+    get totalPrice() { return this.price * this.quantity }; 
 }
--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -5,9 +5,10 @@ export class ShoppingCart {
   items: ShoppingCartItem[] = [];
 
   constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) { 
+    this.itemsMap = itemsMap || {};
     for (const productId in itemsMap) {
       let item = itemsMap[productId];
-      this.items.push(new ShoppingCartItem(item.product, item.quantity)) ;
+      this.items.push(new ShoppingCartItem({ ...item, $key: productId }));
     }
   }
 
@@ -15,6 +16,13 @@ export class ShoppingCart {
     
     return Object.keys(this.itemsMap);
   }
+
+  getQuantity(product: Product) {
+    let item = this.itemsMap[product.$key];
+    return item ? item.quantity : 0;
+    
+  }
+
   get totalItemsCount() {
     let count = 0;
     for(let productId in this.itemsMap) 
@@ -29,10 +37,5 @@ export class ShoppingCart {
     return sum;
   }
 
-  getQuantity(product: Product) {
-    console.log('prduct in shotpping car', product);
-    let item = this.itemsMap[product.$key];
-    console.log('items map ', this.itemsMap);
-    return item ? item.quantity : 0;
-  }
+  
 }
