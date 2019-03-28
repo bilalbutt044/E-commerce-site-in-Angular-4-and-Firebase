a51af469b46ed4682f9eee09c3ee0bdfb0edd53f
bilal <bilal.soft000@gmail.com>
2019-03-28 18:58:55 +0500

    Build a shipping form save order in firebase added userid in Order object in firebase

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { ShoppingCart } from '../models/shopping-cart';
+import { Subscription } from 'rxjs/Subscription';
+import { OrderService } from '../order.service';
+import { AuthService } from '../auth.service';
+import { Order } from '../models/order';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  shipping= {};
+  cart: ShoppingCart;
+  cartSubscription: Subscription;
+  userSubscription: Subscription;
+  userId: string;
 
-  ngOnInit() {
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService,
+    private shoppingCartService: ShoppingCartService) { }
+
+  async ngOnInit() {
+    let cart$ = await this.shoppingCartService.getCart();
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
   }
 
+  ngOnDestroy() {
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
+  }
+  placeOrder() {
+    let order = new Order(this.userId, this.shipping, this.cart);
+    this.orderService.storeOrder(order);
+  }
 }
